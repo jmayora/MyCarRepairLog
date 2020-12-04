99ecde143f87d82ef7a337b39b20bcfa4f63ed47
Justo Mayora <justo.mayora@gmail.com>
2020-12-03 19:22:01 -0500

    add Auto class add insert car

--- app/src/main/java/com/example/mycarrepairlog/AddCarActivity.java
+++ app/src/main/java/com/example/mycarrepairlog/AddCarActivity.java
@@ -3,12 +3,40 @@ package com.example.mycarrepairlog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AddCarActivity extends AppCompatActivity {
+
+    TextView editTxtCarBrand, editTxtCarModel, editTxtCarYear, editTxtCarKilometers;
+    Button btnInsertCar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_car);
+
+        editTxtCarBrand = findViewById(R.id.editTxtCarBrand);
+        editTxtCarModel = findViewById(R.id.editTxtCarModel);
+        editTxtCarYear = findViewById(R.id.editTxtCarYear);
+        editTxtCarKilometers = findViewById(R.id.editTxtCarKilometers);
+        btnInsertCar = findViewById(R.id.btnInsertCar);
+
+    }
+
+
+    public void insertCar(View view){
+
+        String carBrand = editTxtCarBrand.getText().toString();
+        String carModel = editTxtCarModel.getText().toString();
+        int carYear = Integer.parseInt(editTxtCarYear.getText().toString());
+        int carKilometers = Integer.parseInt(editTxtCarKilometers.getText().toString());
+
+        Auto auto = new Auto(carBrand, carModel, carYear, carKilometers);
+
+        Log.d("Auto Info: ", auto.toString());
+
     }
 }
---
+++ app/src/main/java/com/example/mycarrepairlog/Auto.java
@@ -0,0 +1,71 @@
+package com.example.mycarrepairlog;
+
+public class Auto {
+
+    private int ID;
+    private String brand;
+    private String model;
+    private int year;
+    private int kilometers;
+
+    public Auto(String brand, String model, int year, int kilometers) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.kilometers = kilometers;
+    }
+
+    public Auto() {
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getKilometers() {
+        return kilometers;
+    }
+
+    public void setKilometers(int kilometers) {
+        this.kilometers = kilometers;
+    }
+
+    @Override
+    public String toString() {
+        return "Auto{" +
+                "ID=" + ID +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", kilometers=" + kilometers +
+                '}';
+    }
+}
